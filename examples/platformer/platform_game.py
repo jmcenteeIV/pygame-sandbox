18ed1382f059e50f65fd045d4a38599fdb697826
Jansen McEntee <jmcenteeiv@gmail.com>
2022-01-10 18:38:29 -0700

    add changes

--- examples/platformer/platform_game.py
+++ examples/platformer/platform_game.py
@@ -1,10 +1,10 @@
+import random
 import pygame
 import sys
+# from examples.platformer import lib
 
+from lib import Platform, Player
 
-from lib.player import Player
-from lib.platform import Platform
- 
 pygame.init()
  
 HEIGHT = 1080
@@ -18,18 +18,34 @@ FramePerSec = pygame.time.Clock()
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game")
 
+
 PT1 = Platform(HEIGHT, WIDTH)
+PT1.surf = pygame.Surface((WIDTH, 20))
+PT1.surf.fill((255,0,0))
+PT1.rect = PT1.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
+
 P1 = Player(HEIGHT, WIDTH, ACC, FRIC)
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(PT1)
 all_sprites.add(P1)
+
+platforms = pygame.sprite.Group()
+platforms.add(PT1)
+
+for x in range(random.randint(5, 6)):
+    pl = Platform(HEIGHT, WIDTH)
+    platforms.add(pl)
+    all_sprites.add(pl)
  
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:    
+            if event.key == pygame.K_SPACE:
+                P1.jump()
      
     displaysurface.fill((0,0,0))
  
@@ -37,6 +53,8 @@ while True:
         displaysurface.blit(entity.surf, entity.rect)
         P1.move()
         P1.draw(displaysurface)
+        hits = pygame.sprite.spritecollide(P1 , platforms, False)
+        P1.resolve_collisions(hits)
  
     pygame.display.update()
     FramePerSec.tick(FPS)
